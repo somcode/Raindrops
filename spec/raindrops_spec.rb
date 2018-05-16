078ee7a7d4e5b074aac18931e97c77616c78845e
Somayeh <mail.somayeh@gmail.com>
2018-05-16 15:07:27 +0100

    Test to create a convert method with one argument

--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -1,4 +1,12 @@
 require 'raindrops'
 describe Raindrops do
 
+  subject(:raindrops) { described_class.new }
+
+  describe '#convert' do
+    it 'responds to method convert with one argument' do
+      expect(raindrops).to respond_to(:convert).with(1).argument
+    end
+  end
+
 end
