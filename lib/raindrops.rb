7fba70e6c520d119e558de0f2e10f36ed8ef0da1
Somayeh <mail.somayeh@gmail.com>
2018-05-23 10:52:08 +0100

    Pass test, output 'PlingPlung' if number has a factor of 9

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -4,6 +4,7 @@ class Raindrops
     result << 'Pling' if (number % 3).zero?
     result << 'Plang' if (number % 5).zero?
     result << 'Plong' if (number % 7).zero?
+    result << 'Plung' if (number % 9).zero?
     result = number.to_s if result.empty?
     result
   end
